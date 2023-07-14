3c66cfdaeb8f2dac774487f93d6f6af06ef05d59
PaulRomanov <pavel.ramanau@gmail.com>
2023-07-14 16:19:09 +0400

    add finished task 01-basics\02-VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,44 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
+const vm = createApp({
+  data() {
+    return {
+      number_1: 0,
+      number_2: 0,
+      operator: 'sum',
+    };
+  },
 
-// Создайте Vue приложение
+  computed: {
+    result() {
+      if (this.operator === 'sum') {
+        return this.number_1 + this.number_2;
+      } else if (this.operator === 'subtract') {
+        return this.number_1 - this.number_2;
+      } else if (this.operator === 'multiply') {
+        return this.number_1 * this.number_2;
+      } else if (this.operator === 'divide') {
+        return this.number_1 / this.number_2;
+      }
+      return 0;
+    },
+  },
+
+  methods: {
+    sum() {
+      this.operator = 'sum';
+      return (this.result = this.number_1 + this.number_2);
+    },
+    subtract() {
+      this.operator = 'subtract';
+      return (this.result = this.number_1 - this.number_2);
+    },
+    multiply() {
+      this.operator = 'multiply';
+      return (this.result = this.number_1 * this.number_2);
+    },
+    divide() {
+      this.operator = 'divide';
+      return (this.result = this.number_1 / this.number_2);
+    },
+  },
+}).mount('#app');
