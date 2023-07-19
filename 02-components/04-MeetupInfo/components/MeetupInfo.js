47e8bd72cefd791100d76d421f6b0c4d49cc5c04
PaulRomanov <pavel.ramanau@gmail.com>
2023-07-19 15:59:06 +0400

    add finished task 02-components\04-MeetupInfo

--- 02-components/04-MeetupInfo/components/MeetupInfo.js
+++ 02-components/04-MeetupInfo/components/MeetupInfo.js
@@ -2,20 +2,33 @@ import { defineComponent } from '../vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupInfo',
+  data() {
+    return {
+      options: { day: 'numeric', month: 'long', year: 'numeric' }, // для выввода даты в формате: 8 мая 2020 г.
+    };
+  },
+  props: {
+    organizer: String,
+    place: String,
+    date: {
+      type: Number,
+      required: true, //обязательный параметр
+    },
+  },
 
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+       {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time datetime="2020-01-01">{{ new Date(date).toLocaleDateString('ru-RU', options) }}</time>
       </li>
     </ul>`,
 });
