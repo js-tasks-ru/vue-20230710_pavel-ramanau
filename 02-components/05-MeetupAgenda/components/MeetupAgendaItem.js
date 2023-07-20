fe7960dcc0ec3697a4c49d2b1925c5e23f0e2165
PaulRomanov <pavel.ramanau@gmail.com>
2023-07-20 15:11:25 +0400

    add finished task 02-components\05-MeetupAgenda

--- 02-components/05-MeetupAgenda/components/MeetupAgendaItem.js
+++ 02-components/05-MeetupAgenda/components/MeetupAgendaItem.js
@@ -1,23 +1,42 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
-// import { agendaItemIcons, agendaItemDefaultTitles } from './meetupService.js';
+import { agendaItemIcons, agendaItemDefaultTitles } from '../meetupService.js';
 
 export default defineComponent({
   name: 'MeetupAgendaItem',
 
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true, //обязательный параметр
+    },
+  },
+
+  computed: {
+    // будет использовать agendaItem.title или значение из agendaItemDefaultTitles, если title отсутствует
+    agendaTitle() {
+      return this.agendaItem.title || agendaItemDefaultTitles[this.agendaItem.type];
+    },
+    agentaIcon(){
+      return '/assets/icons/icon-' + agendaItemIcons[this.agendaItem.type] + '.svg';
+    },
+  },
+  
   template: `
     <div class="agenda-item">
       <div class="agenda-item__col">
-        <img src="/assets/icons/icon-key.svg" class="icon" alt="key" />
+        <img :src="agentaIcon" class="icon" :alt="agentaIcon" />
+        <!-- эта шляпа не работает -->
+        <!-- <img src="/assets/icons/icon-key.svg" class="icon" :alt="\`\${agentaIcon}\`" /> -->
       </div>
-      <div class="agenda-item__col">00:00 - 00:00</div>
+      <div class="agenda-item__col">{{ agendaItem.startsAt }} - {{ agendaItem.endsAt }}</div>
       <div class="agenda-item__col">
-        <h3 class="agenda-item__title">Title</h3>
-        <p class="agenda-item__talk">
-          <span>Talk Speaker</span>
+        <h3 class="agenda-item__title">{{ agendaTitle }}</h3>
+       <p v-if="agendaItem.type === 'talk'" class="agenda-item__talk" >  
+          <span>{{ agendaItem.speaker }}</span>
           <span class="agenda-item__dot"></span>
-          <span class="agenda-item__lang">EN</span>
+          <span class="agenda-item__lang">{{ agendaItem.language }}</span>
         </p>
-        <p>Description</p>
+        <p>{{ agendaItem.description }}</p>
       </div>
     </div>`,
 });
