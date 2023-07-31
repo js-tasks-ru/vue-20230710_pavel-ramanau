a86b55d207103c61c2756625e125392aec4663cd
PaulRomanov <pavel.ramanau@gmail.com>
2023-07-31 13:53:35 +0400

    delete tast

--- 04-vue-cli/04-MeetupsCalendar/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import '@/assets/styles/app.css';
-import '@/assets/styles/taskbook.css';
-import '@/assets/styles/_container.css';
-
-createApp(App).mount('#app');
