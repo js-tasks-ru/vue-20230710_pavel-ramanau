1dcf8e5672131c545c8824a17ef6c5e9950aeda8
PaulRomanov <pavel.ramanau@gmail.com>
2023-08-04 12:24:29 +0400

    add  some finished mini tasks

---
+++ 04-vue-cli/04-MeetupsCalendar/components/MeetupsCalendar.vue
@@ -0,0 +1,243 @@
+<template>
+  <div class="calendar-view">
+    <div class="calendar-view__controls">
+      <div class="calendar-view__controls-inner">
+        <button
+          class="calendar-view__control-left"
+          type="button"
+          aria-label="Previous month"
+          @click="previousMonth"
+        ></button>
+        <div class="calendar-view__date">{{ titelData }}</div>
+        <button class="calendar-view__control-right" type="button" aria-label="Next month" @click="nextMonth"></button>
+      </div>
+    </div>
+
+    <div class="calendar-view__grid">
+      <!-- нужно отобразить через v-for -->
+      <!-- calendar-view__cell_inactive добавляем класс только если isCurrentMonth: false -->
+      <div
+        class="calendar-view__cell"
+        :class="{ 'calendar-view__cell_inactive': !day.isCurrentMonth }"
+        tabindex="0"
+        v-for="(day, index) in currentMonthDays"
+        :key="index"
+      >
+        <div class="calendar-view__cell-day">{{ day.day }}</div>
+        <div class="calendar-view__cell-content"></div>
+      </div>
+
+      <!-- <div class="calendar-view__cell" tabindex="0">
+        <div class="calendar-view__cell-day">12</div>
+        <div class="calendar-view__cell-content">
+          <a href="/meetups/1" class="calendar-event">Meetup 1</a>
+          <a href="/meetups/2" class="calendar-event">Meetup 2</a>
+        </div>
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MeetupsCalendar',
+
+  data() {
+    return {
+      date: new Date(), // Получаем текущую дату
+      //вывод заголовка календаря
+      titelData: new Date().toLocaleDateString('RU', {
+        month: 'long',
+        year: 'numeric',
+      }),
+    };
+  },
+
+  props: {
+    meetups: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  computed: {
+    //формирует дни текущего месяца
+    currentMonthDays() {
+      const currentYear = this.date.getFullYear(); // Текущий год
+      const currentMonth = this.date.getMonth(); // Текущий месяц, нумерация месяцев начинается с 0 до 11
+
+      const firstDay = new Date(currentYear, currentMonth, 1); // Первый день текущего месяца (год, месяц, номер дня)
+      const lastDay = new Date(currentYear, currentMonth + 1, 0); // Последний день текущего месяца.
+
+      let days = [];
+
+      // Перебираем дни от первого до последнего
+      for (let date = firstDay; date <= lastDay; date.setDate(date.getDate() + 1)) {
+        const day = date.getDate(); // получаем число (день месяца)
+        days.push({
+          day,
+          date: +date, // Преобразуем дату в милисекунды
+          isCurrentMonth: date.getMonth() === currentMonth, // Флаг, указывающий, что день принадлежит текущему месяцу
+        });
+      }
+      return days; //массив объектов  {day: 1, date: 1690833600000, isCurrentMonth: true}
+    },
+
+
+  },
+
+  methods: {
+    //определяет первый и последний день текущего месяца на основе текущей даты
+    // getMonthRange(date) {},
+
+    nextMonth() {
+      // получаем дату следующего месяца
+      this.date = new Date(this.date.setMonth(this.date.getMonth() + 1));
+      this.getTitelData()
+    },
+    previousMonth() {
+      // получаем дату предыдущего месяца
+      this.date = new Date(this.date.setMonth(this.date.getMonth() - 1));
+      this.getTitelData()
+    },
+
+    getTitelData() { // изметяет titelData
+    this.titelData = this.date.toLocaleDateString('RU', {
+        month: 'long',
+        year: 'numeric',
+      })
+    },
+
+  },
+};
+</script>
+
+<style scoped>
+.calendar-view {
+}
+
+.calendar-view__controls {
+  text-align: center;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 1;
+  color: var(--blue);
+  background-color: var(--blue-extra);
+  padding: 24px;
+  display: flex;
+  justify-content: center;
+}
+
+.calendar-view__controls-inner {
+  max-width: 325px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  text-transform: capitalize;
+}
+
+.calendar-view__controls-inner button {
+  border: none;
+  padding: 0;
+}
+
+.calendar-view__control-left,
+.calendar-view__control-right {
+  width: 30px;
+  height: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  cursor: pointer;
+  transition: 0.3s all;
+  background: url('@/assets/icons/icon-pill-active.svg') left center no-repeat;
+  background-size: cover;
+}
+
+.calendar-view__control-left:hover,
+.calendar-view__control-right:hover {
+  opacity: 0.8;
+}
+
+.calendar-view__control-right {
+  transform: rotate(180deg);
+}
+
+.calendar-view__grid {
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+}
+
+.calendar-view__grid {
+  border: 1px solid var(--grey);
+  border-bottom: none;
+}
+
+.calendar-view__cell {
+  position: relative;
+  height: auto;
+  padding: 6px 8px;
+  background-color: var(--white);
+  color: var(--grey-8);
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  border-bottom: 1px solid var(--grey);
+  border-left: 1px solid var(--grey);
+  text-align: right;
+}
+
+.calendar-view__cell.calendar-view__cell_inactive {
+  background-color: var(--grey-light);
+}
+
+@media all and (max-width: 767px) {
+  .calendar-view__cell:nth-child(5n + 1) {
+    border-left: none;
+  }
+}
+
+@media all and (min-width: 767px) {
+  .calendar-view__grid {
+    grid-template-columns: repeat(7, 1fr);
+  }
+
+  .calendar-view__cell {
+    height: 144px;
+  }
+
+  .calendar-view__cell:nth-child(7n + 1) {
+    border-left: none;
+  }
+}
+
+.calendar-event {
+  --max-lines: 2;
+  --line-height: 16px;
+
+  display: block;
+  text-align: left;
+  text-decoration: none;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: var(--line-height);
+  color: var(--white);
+  padding: 4px 6px;
+  border-radius: 2px;
+  background-color: var(--blue);
+  margin-top: 4px;
+}
+
+@media all and (min-width: 767px) {
+  .calendar-event {
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    max-height: calc(var(--max-lines) * var(--line-height) + 6px);
+  }
+}
+</style>
