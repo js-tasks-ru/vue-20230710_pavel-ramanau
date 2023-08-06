ab6f4700ab47aea546cea80f0ac8e769306f5b76
PaulRomanov <pavel.ramanau@gmail.com>
2023-08-06 15:07:27 +0400

    add finished task 05-vue-router\04-UiTabsLink

--- 05-vue-router/04-UiTabsLink/components/UiTabsLink.vue
+++ 05-vue-router/04-UiTabsLink/components/UiTabsLink.vue
@@ -1,10 +1,25 @@
 <template>
-  <a class="tabs__tab tabs__tab_active" role="tab">Tab</a>
+  <RouterLink :to="to" class="tabs__tab" :class="{ tabs__tab_active: isActive }" role="tab">
+    <slot />
+  </RouterLink>
 </template>
 
 <script>
 export default {
   name: 'UiTabsLink',
+
+  props: {
+    to: [String, Object],
+  },
+
+  computed: {
+    isActive() {
+      // позволяет получить объект с информацией о том, как будет обработан переданный маршрут, без непосредственной навигации.
+      // {fullPath: '/meetups/2', hash: '', query: {…}, name: 'meetup.description', path: '/meetups/2',…}
+      const resolvedRoute = this.$router.resolve(this.to);
+      return resolvedRoute.fullPath === this.$route.fullPath;
+    },
+  },
 };
 </script>
 
