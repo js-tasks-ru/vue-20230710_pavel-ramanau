84221e925d1280f3b4262d091a0036698d86ef2e
PaulRomanov <pavel.ramanau@gmail.com>
2023-08-27 17:39:54 +0400

    add task 06-wrappers\06-UiInputDate

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,13 +1,77 @@
 <template>
-  <UiInput />
+  <UiInput
+    :type="type"
+    :step="step"
+    :modelValue="formattedModelValue"
+    @input="updateModelValue"
+  >
+    <template v-for="slotName in Object.keys($slots)" #[slotName]>
+      <slot :name="slotName" />
+    </template>
+  </UiInput>
 </template>
 
 <script>
-import UiInput from './UiInput.vue';
+import UiInput from "./UiInput.vue";
 
 export default {
-  name: 'UiInputDate',
+  name: "UiInputDate",
+  props: {
+    type: {
+      type: String,
+      default: "date",
+      validator(value) {
+        return ["date", "time", "datetime-local"].includes(value);
+      },
+    },
+    modelValue: {
+      type: [Number, String, null],
+      default: null,
+    },
+    step: {
+      type: String,
+      default: "any",
+    },
+  },
 
   components: { UiInput },
+
+  emits: ["update:modelValue"],
+
+  computed: {
+    formattedModelValue() {
+      if (this.type === "date" && this.modelValue !== null) {
+        return new Date(this.modelValue).toISOString().slice(0, 10);
+      } else if (this.type === "time" && this.modelValue !== null) {
+        return new Date(this.modelValue).toISOString().slice(11, 16);
+      } else if (this.type === "datetime-local" && this.modelValue !== null) {
+        return new Date(this.modelValue).toISOString().slice(0, 16);
+      } else {
+        return "";
+      }
+    },
+  },
+
+  methods: {
+    updateModelValue(event) {
+      const newValue = event.target.value;
+
+      if (newValue === "") {
+        this.$emit("update:modelValue", null);
+      } else if (this.type === "date" && newValue !== null && newValue !== undefined) {
+        this.$emit("update:modelValue", Date.parse(newValue));
+      } else if (this.type === "time" && newValue !== null && newValue !== undefined) {
+        this.$emit("update:modelValue", Date.parse(`1970-01-01T${newValue}`));
+      } else if (
+        this.type === "datetime-local" &&
+        newValue !== null &&
+        newValue !== undefined
+      ) {
+        this.$emit("update:modelValue", Date.parse(newValue));
+      } else {
+        this.$emit("update:modelValue", newValue);
+      }
+    },
+  },
 };
 </script>
