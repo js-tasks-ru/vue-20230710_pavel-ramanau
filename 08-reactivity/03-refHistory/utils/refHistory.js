297299b7d0535a52b2ca1b72a24e06eecefd5382
PaulRomanov <pavel.ramanau@gmail.com>
2023-08-31 15:54:55 +0400

    add  08-reactivity\03-refHistory

--- 08-reactivity/03-refHistory/utils/refHistory.js
+++ 08-reactivity/03-refHistory/utils/refHistory.js
@@ -1,12 +1,25 @@
-import { ref } from 'vue';
+import { ref, watch, watchEffect } from 'vue';
 
 /**
  * @template T
  * @param {Ref<T>} source - Отслеживаемый ref
  * @returns {Object<{ history: Ref<T[]> }>} - История изменения source
  */
+
 export function refHistory(source) {
-  const history = ref();
-  // ...
+  const history = ref([]); //реактивный массив
+
+  const addToHistory = () => {
+    history.value.push(source.value);
+  };
+
+  watchEffect(
+    () => {
+      addToHistory();
+      source.value = history.value[history.value.length - 1];
+    },
+    { flush: 'sync' }, //обработчик будет вызываться сразу же после изменения значения
+  );
+
   return { history };
 }
