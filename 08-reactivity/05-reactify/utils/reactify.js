1db8a68bd0987a7170603fab2323b280264f37d7
PaulRomanov <pavel.ramanau@gmail.com>
2023-09-01 20:33:54 +0400

    add  08-reactivity\05-reactify

--- 08-reactivity/05-reactify/utils/reactify.js
+++ 08-reactivity/05-reactify/utils/reactify.js
@@ -1,4 +1,4 @@
-import { computed } from 'vue';
+import { computed, ref, isRef } from 'vue';
 
 /**
  * @template T
@@ -6,6 +6,12 @@ import { computed } from 'vue';
  * @returns {function(...[*]): ComputedRef<T>} - Функция вычисления от ref-ов, возвращающая вычисляемое значение computed
  */
 export function reactify(func) {
-  // ...
-  // return () => computed(() => {});
+    //принимает неопределенное количество аргументов
+  return (...args) => {
+    //оборачиваем аргумент в ref, если это не ref
+    const refArgs = args.map((arg) => (isRef(arg) ? arg : ref(arg)));
+
+    const computedValue = computed(() => func(...refArgs.map((arg) => (isRef(arg) ? arg.value : arg))));
+    return computedValue;
+  };
 }
