153721ac0bc56a5679c25024dd95cf67c9be8732
PaulRomanov <pavel.ramanau@gmail.com>
2023-09-03 20:41:58 +0400

    teacher problem solving

--- 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
+++ 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
@@ -1,5 +1,5 @@
 <script>
-// import { compile } from 'vue';
+import { compile, defineComponent, h } from 'vue';
 
 export default {
   name: 'TemplateRenderer',
@@ -19,6 +19,28 @@ export default {
       type: [Object, Array],
       default: () => [],
     },
+  },
+
+  computed: {
+    // Рендерим шаблон в render-функцию
+    renderFunction() {
+      return compile(this.template);
+    },
+
+    // Генерируем компонент с этой рендер функцией и зарегистрированными компонентами
+    componentFromTemplate() {
+      return defineComponent({
+        name: 'TemplateRendererInternal',
+        components: this.components,
+        props: ['bindings'],
+        render: this.renderFunction,
+      });
+    },
+  },
+
+  render() {
+    // Рендерим сгенерированный компонент, передавая в него данные
+    return h(this.componentFromTemplate, { bindings: this.bindings });
   },
 };
 </script>
