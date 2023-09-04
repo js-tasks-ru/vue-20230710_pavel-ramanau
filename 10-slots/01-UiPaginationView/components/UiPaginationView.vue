f0f1fbac9be92ccafccd5c8208ec6c3bdcfe16b7
PaulRomanov <pavel.ramanau@gmail.com>
2023-09-04 13:34:21 +0400

    add 10-slots\01-UiPaginationView

---
+++ 10-slots/01-UiPaginationView/components/UiPaginationView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="pagination-container" v-for="item in paginatedItems" :key="item">
+    <slot :item="item" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UiPaginationView',
+
+  props: {
+    page: {
+      type: Number,
+      required: true,
+      validator: (value) => value > 0,
+    },
+
+    perPage: {
+      type: Number,
+      required: true,
+      validator: (value) => value > 0,
+    },
+
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  computed: {
+    paginatedItems() {
+      const startIndex = (this.page - 1) * this.perPage;
+      const endIndex = startIndex + this.perPage;
+      return this.items.slice(startIndex, endIndex);
+    },
+  },
+};
+</script>
+
+<style></style>
