ab72e4e05505032de1c2425f293768e877003343
PaulRomanov <pavel.ramanau@gmail.com>
2023-09-04 15:36:08 +0400

    add 10-slots\02-PromiseWrapper

--- 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
+++ 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
@@ -1,5 +1,9 @@
 <template>
-  <!-- -->
+  <div>
+    <slot v-if="currentState === 'pending'" name="pending"></slot>
+    <slot v-else-if="currentState === 'fulfilled'" name="fulfilled" :result="result"></slot>
+    <slot v-else-if="currentState === 'rejected'" name="rejected" :error="error"></slot>
+  </div>
 </template>
 
 <script>
@@ -10,6 +14,35 @@ export default {
     promise: {
       type: Promise,
       required: true,
+    },
+  },
+
+  data() {
+    return {
+      currentState: 'pending',
+      result: null,
+      error: null,
+    };
+  },
+
+  watch: {
+    immediate: true,
+    promise: {
+      handler(newPromise) {
+        this.currentState = 'pending';
+        this.result = null;
+        this.error = null;
+
+        newPromise
+          .then(result => {
+            this.currentState = 'fulfilled';
+            this.result = result;
+          })
+          .catch(error => {
+            this.currentState = 'rejected';
+            this.error = error;
+          });
+      },
     },
   },
 };
